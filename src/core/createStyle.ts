6bf59ee98f1b672eda72250f2c775fa7c4fe9dd1
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-08-20 15:42:47 +0200

    add core methods to merge styles and to use classes from outside the lib

---
+++ src/core/createStyle.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import { mainStyles } from './mainStyles';
+import { Styles, StyleKeys } from './types';
+
+export const s = (...classes: StyleKeys[]): Styles => {
+  const flattenClasses = classes.reduce<Styles[]>((pv, cv) => [...pv, mainStyles[cv]], []);
+  return StyleSheet.flatten(flattenClasses)
+};
