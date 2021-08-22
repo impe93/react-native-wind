e33d9694e7b92f87fa2ce98634992201d2d14efe
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-08-23 01:21:54 +0200

    update dist folder

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,3 +1,5 @@
-import { MarginStyles } from "./styles/spacing/margins";
+import { MarginClass, MarginStyles } from './styles/spacing/margins';
+import { PaddingClass, PaddingStyles } from './styles/spacing/paddings';
 export declare type Valueof<T> = T[keyof T];
-export declare type Styles = MarginStyles;
+export declare type Styles = MarginStyles | PaddingStyles;
+export declare type StyleClass = MarginClass | PaddingClass;
