cbeeee34f5131e0ae7f36946555b1726b86136e0
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-08-23 22:07:35 +0200

    add justify content classes

---
+++ src/styles/flex/justify-content.ts
@@ -0,0 +1,20 @@
+export const justifyContent = {
+  'justify-start': {
+    justifyContent: 'flex-start',
+  },
+  'justify-end': {
+    justifyContent: 'flex-end',
+  },
+  'justify-center': {
+    justifyContent: 'center',
+  },
+  'justify-between': {
+    justifyContent: 'space-between',
+  },
+  'justify-around': {
+    justifyContent: 'space-around',
+  },
+  'justify-evenly': {
+    justifyContent: 'space-evenly',
+  },
+} as const;
