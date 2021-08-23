29be469799444a3a1a92f1db5ab9fabfded9b6de
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-08-24 00:04:07 +0200

    add align self classes

---
+++ src/styles/flex/align-self.ts
@@ -0,0 +1,20 @@
+export const alignSelf = {
+  'self-center': {
+    alignSelf: 'center'
+  },
+  'self-start': {
+    alignSelf: 'flex-start'
+  },
+  'self-end': {
+    alignSelf: 'flex-end'
+  },
+  'self-baseline': {
+    alignSelf: 'baseline'
+  },
+  'self-stretch': {
+    alignSelf: 'stretch'
+  },
+  'self-auto': {
+    alignSelf: 'auto'
+  },
+} as const;
