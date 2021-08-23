40285b77b774599bd01b19ea79171c1e97d4ee29
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-08-23 21:48:16 +0200

    add flex direction utility

---
+++ src/styles/flex/flex-direction.ts
@@ -0,0 +1,14 @@
+export const flexDirections = {
+  'flex-row': {
+    flexDirection: 'row',
+  },
+  'flex-row-reverse': {
+    flexDirection: 'row-reverse',
+  },
+  'flex-column': {
+    flexDirection: 'column',
+  },
+  'flex-column-reverse': {
+    flexDirection: 'column-reverse',
+  },
+} as const;
