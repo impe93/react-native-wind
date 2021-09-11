c61eaa3d2e55a4edc46a5e0e14eb761f101f0055
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-09-11 12:12:32 +0200

    change how s function work

--- src/core/createStyle.ts
+++ src/core/createStyle.ts
@@ -2,8 +2,8 @@ import { StyleProp, StyleSheet } from 'react-native';
 import { mainStyles } from './mainStyles';
 import { Styles } from '../types';
 
-export const s = (...classes: string[]): StyleProp<any> => {
-  const flattenClasses = classes.reduce<Styles[]>(
+export const s = (classes: string): StyleProp<any> => {
+  const flattenClasses = classes.split(' ').reduce<Styles[]>(
     (pv, cv) => [...pv, mainStyles[cv]] as Styles[],
     [],
   );
