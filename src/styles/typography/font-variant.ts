75384a5548e40196e4a3b8bfa0dae04b55c15c83
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-10-10 13:06:17 +0200

    add font-variants to utilities

---
+++ src/styles/typography/font-variant.ts
@@ -0,0 +1,17 @@
+export const fontVariants = {
+  'lining-nums': {
+    fontVariant: ['lining-nums'],
+  },
+  'oldstyle-nums': {
+    fontVariant: ['oldstyle-nums'],
+  },
+  'proportional-nums': {
+    fontVariant: ['proportional-nums'],
+  },
+  'small-caps': {
+    fontVariant: ['small-caps'],
+  },
+  'tabular-nums': {
+    fontVariant: ['tabular-nums'],
+  },
+} as const;
--- src/styles/typography/typography.ts
+++ src/styles/typography/typography.ts
@@ -1,20 +1,24 @@
 import { fontSizes } from './font-size';
 import { fontStyles } from './font-style';
 import { fontWeights } from './font-weight';
+import { fontVariants } from './font-variant';
 
 export type TypographyClass =
   | keyof typeof fontSizes
   | keyof typeof fontStyles
-  | keyof typeof fontWeights;
+  | keyof typeof fontWeights
+  | keyof typeof fontVariants;
 
 export type TypographyStyle = typeof fontSizes &
   typeof fontStyles &
-  typeof fontWeights;
+  typeof fontWeights &
+  typeof fontVariants;
 
 export const buildTypography = (): TypographyStyle => {
   return {
     ...fontSizes,
     ...fontStyles,
     ...fontWeights,
+    ...fontVariants,
   } as const;
 };
