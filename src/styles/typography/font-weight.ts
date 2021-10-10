ae0665bee96f5bb943e908dc93f2f07f38a42837
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-10-10 12:55:25 +0200

    add font-weights utilities

---
+++ src/styles/typography/font-weight.ts
@@ -0,0 +1,29 @@
+export const fontWeights = {
+  'font-thin': {
+    fontWeight: '100',
+  },
+  'font-extralight': {
+    fontWeight: '200',
+  },
+  'font-light': {
+    fontWeight: '300',
+  },
+  'font-normal': {
+    fontWeight: '400',
+  },
+  'font-medium': {
+    fontWeight: '500',
+  },
+  'font-semibold': {
+    fontWeight: '600',
+  },
+  'font-bold': {
+    fontWeight: '700',
+  },
+  'font-extrabold': {
+    fontWeight: '800',
+  },
+  'font-black': {
+    fontWeight: '900',
+  },
+} as const;
--- src/styles/typography/typography.ts
+++ src/styles/typography/typography.ts
@@ -1,13 +1,20 @@
 import { fontSizes } from './font-size';
 import { fontStyles } from './font-style';
+import { fontWeights } from './font-weight';
 
-export type TypographyClass = keyof typeof fontSizes | keyof typeof fontStyles;
+export type TypographyClass =
+  | keyof typeof fontSizes
+  | keyof typeof fontStyles
+  | keyof typeof fontWeights;
 
-export type TypographyStyle = typeof fontSizes & typeof fontStyles;
+export type TypographyStyle = typeof fontSizes &
+  typeof fontStyles &
+  typeof fontWeights;
 
 export const buildLayout = (): TypographyStyle => {
   return {
     ...fontSizes,
     ...fontStyles,
+    ...fontWeights,
   } as const;
 };
