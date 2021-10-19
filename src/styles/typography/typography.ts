8c1ec31ba5745c5a3edc43dedef9781e678dd991
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-10-20 00:45:12 +0200

    add letter spacing

--- src/styles/typography/typography.ts
+++ src/styles/typography/typography.ts
@@ -2,17 +2,20 @@ import { fontSizes } from './font-size';
 import { fontStyles } from './font-style';
 import { fontWeights } from './font-weight';
 import { fontVariants } from './font-variant';
+import { letterSpacing } from './letter-spacing';
 
 export type TypographyClass =
   | keyof typeof fontSizes
   | keyof typeof fontStyles
   | keyof typeof fontWeights
-  | keyof typeof fontVariants;
+  | keyof typeof fontVariants
+  | keyof typeof letterSpacing;
 
 export type TypographyStyle = typeof fontSizes &
   typeof fontStyles &
   typeof fontWeights &
-  typeof fontVariants;
+  typeof fontVariants &
+  typeof letterSpacing;
 
 export const buildTypography = (): TypographyStyle => {
   return {
@@ -20,5 +23,6 @@ export const buildTypography = (): TypographyStyle => {
     ...fontStyles,
     ...fontWeights,
     ...fontVariants,
+    ...letterSpacing,
   } as const;
 };
