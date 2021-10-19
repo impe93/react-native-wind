b628ff50c1c060adf8bb673d82b8b107d9ff460e
Lorenzo Imperatrice <lorenzo.imperatrice@gmail.com>
2021-10-20 00:48:21 +0200

    add text align

---
+++ src/styles/typography/text-align.ts
@@ -0,0 +1,17 @@
+export const textAlign = {
+  'text-left': {
+    textAlign: 'left',
+  },
+  'text-center': {
+    textAlign: 'center',
+  },
+  'text-right': {
+    textAlign: 'right',
+  },
+  'text-justify': {
+    textAlign: 'justify',
+  },
+  'text-auto': {
+    textAlign: 'auto',
+  },
+} as const;
--- src/styles/typography/typography.ts
+++ src/styles/typography/typography.ts
@@ -3,18 +3,21 @@ import { fontStyles } from './font-style';
 import { fontWeights } from './font-weight';
 import { fontVariants } from './font-variant';
 import { letterSpacing } from './letter-spacing';
+import { textAlign } from './text-align';
 
 export type TypographyClass =
   | keyof typeof fontSizes
   | keyof typeof fontStyles
   | keyof typeof fontWeights
   | keyof typeof fontVariants
-  | keyof typeof letterSpacing;
+  | keyof typeof letterSpacing
+  | keyof typeof textAlign;
 
 export type TypographyStyle = typeof fontSizes &
   typeof fontStyles &
   typeof fontWeights &
   typeof fontVariants &
+  typeof textAlign &
   typeof letterSpacing;
 
 export const buildTypography = (): TypographyStyle => {
@@ -24,5 +27,6 @@ export const buildTypography = (): TypographyStyle => {
     ...fontWeights,
     ...fontVariants,
     ...letterSpacing,
+    ...textAlign,
   } as const;
 };
